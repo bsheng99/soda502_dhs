721ef658c15e9d023b851cb3e78023758d2e6868
bsheng99 <bsheng99@gmail.com>
2016-10-20 11:24:47 -0400

    Add files via upload

--- MapFertilityRateJapan.R
+++ MapFertilityRateJapan.R
@@ -8,14 +8,13 @@ library(data.table)
 
 #CRS.WGS84 = CRS("+proj=longlat +datum=WGS84 +no_defs +ellps=WGS84 +towgs84=0,0,0")
 
-filepath = 'soda502_dhs-master/Japan TFR.csv'
-data <- fread(paste(filepath))
+data <- read.csv("Japan TFR.csv")
 
 # Format geographical names; remove numbers, space, drop total, sort df alphabetically
-data$V1 = gsub('[0-9]+', '', tolower(data$V1))
-data$V1 = gsub("^\\s+|\\s+$", "", data$V1)
-data = data[3:length(data$V1)]
-data <- with(data,  data[order(data$V1) , ])
+data$Prefecture = gsub('[0-9]+', '', tolower(data$Prefecture))
+data$Prefecture = gsub("^\\s+|\\s+$", "", data$Prefecture)
+data = data[2:length(data$Prefecture),]
+data <- with(data,  data[order(data$Prefecture) , ])
 
 ## Map
 
@@ -29,17 +28,48 @@ ggplot(japan.map, aes(long, lat, group=group)) +
   ggtitle("Japan")
 data(df_japan_census)
 
+data$change0607 <- data[,3]-data[,2]
+data$change0708 <- data[,4]-data[,3]
+data$change0809 <- data[,5]-data[,4]
+data$change0910 <- data[,6]-data[,5]
+data$change1011 <- data[,7]-data[,6]
+data$change1112 <- data[,8]-data[,7]
+data$change1213 <- data[,9]-data[,8]
 
-df_japan_census$value = data$V6
-admin1_choropleth("japan", df_japan_census, num_colors = 5, title = 'Japanese Fertility by Prefecture in 2010', legend = "Total Fertility Rate")
+df_japan_census$value = data[,5]
+admin1_choropleth("japan", df_japan_census, num_colors = 1, title = 'Japanese Fertility by Prefecture in 2009', legend = "Total Fertility Rate")
 
-df_japan_census$value = data$V7
-admin1_choropleth("japan", df_japan_census, num_colors = 5, title = 'Japanese Fertility by Prefecture in 2011', legend = "Total Fertility Rate")
+df_japan_census$value = data[,6]
+admin1_choropleth("japan", df_japan_census, num_colors = 1, title = 'Japanese Fertility by Prefecture in 2010', legend = "Total Fertility Rate")
 
-df_japan_census$value = data$V8
-admin1_choropleth("japan", df_japan_census, num_colors = 5, title = 'Japanese Fertility by Prefecture in 2012', legend = "Total Fertility Rate")
+df_japan_census$value = data[,7]
+admin1_choropleth("japan", df_japan_census, num_colors = 1, title = 'Japanese Fertility by Prefecture in 2011', legend = "Total Fertility Rate")
 
-df_japan_census$value = data$V9
-admin1_choropleth("japan", df_japan_census, num_colors = 5, title = 'Japanese Fertility by Prefecture in 2013', legend = "Total Fertility Rate")
+df_japan_census$value = data[,8]
+admin1_choropleth("japan", df_japan_census, num_colors = 1, title = 'Japanese Fertility by Prefecture in 2012', legend = "Total Fertility Rate")
 
+df_japan_census$value = data[,9]
+admin1_choropleth("japan", df_japan_census, num_colors = 1, title = 'Japanese Fertility by Prefecture in 2013', legend = "Total Fertility Rate")
+
+
+df_japan_census$value = data[,"change0607"]
+admin1_choropleth("japan", df_japan_census, num_colors = 1, title = 'Change in Japanese Fertility by Prefecture 2006-2007', legend = "Change in TFR")
+
+df_japan_census$value = data[,"change0708"]
+admin1_choropleth("japan", df_japan_census, num_colors = 1, title = 'Change in Japanese Fertility by Prefecture 2007-2008', legend = "Change in TFR")
+
+df_japan_census$value = data[,"change0809"]
+admin1_choropleth("japan", df_japan_census, num_colors = 1, title = 'Change in Japanese Fertility by Prefecture 2008-2009', legend = "Change in TFR")
+
+df_japan_census$value = data[,"change0910"]
+admin1_choropleth("japan", df_japan_census, num_colors = 1, title = 'Change in Japanese Fertility by Prefecture 2009-2010', legend = "Change in TFR")
+
+df_japan_census$value = data[,"change1011"]
+admin1_choropleth("japan", df_japan_census, num_colors = 1, title = 'Change in Japanese Fertility by Prefecture 2010-2011', legend = "Change in TFR")
+
+df_japan_census$value = data[,"change1112"]
+admin1_choropleth("japan", df_japan_census, num_colors = 1, title = 'Change in Japanese Fertility by Prefecture 2011-2012', legend = "Change in TFR")
+
+df_japan_census$value = data[,"change1213"]
+admin1_choropleth("japan", df_japan_census, num_colors = 1, title = 'Change in Japanese Fertility by Prefecture 2012-2013', legend = "Change in TFR")
 
